42450373c6c51f9df10528659f9630c257dea83d
worashf <uppert83@gmail.com>
2022-05-18 16:47:18 +0300

    add stickey header script

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,15 @@
  let menu = document.getElementById("hamburger");
  let header =document.getElementById("main-header");
  let  mainNav =document.getElementById("main-nav")
+ const sticky = header.offsetTop;
+
+ window.onscroll = () => {
+    if (window.pageYOffset >= sticky) {
+      header.style.top = 0;
+    } else {
+      header.style.top = '5%';
+    }
+  };
 
  function toggleMenu(){
      header.classList.toggle("menu-overlay")
