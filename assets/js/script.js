0c7e406b6d4b492d655b06b19a1205d5ad524028
worashf <uppert83@gmail.com>
2022-05-19 00:45:31 +0300

    add our team object, create team function

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,56 @@
+ const teamMembers =[
+   {
+      name:"Worash Abocherugn",
+      profilePic:"/assets/images/worash.jpeg",
+      position:"Founder and CTO",
+      rule :"Practice practice practice build skills, work smart get the job done. "
+
+
+   },{
+    name:"Dagmawi zewudu",
+    profilePic:"/assets/images/dagm.jpeg",
+    position:"Founder and CTO",
+    rule :"Practice practice practice build skills, work smart get the job done. "
+
+   },{
+    name:"Addis Kebede",
+    profilePic:"/assets/images/addis.jpeg",
+    position:"Founder and CTO",
+    rule :"Practice practice practice build skills, work smart get the job done. "
+
+   },{
+    name:"Sntayhu Boyamo",
+    profilePic:"/assets/images/snte.jpeg",
+    position:"Founder and CTO",
+    rule :"Practice practice practice build skills, work smart get the job done. "
+
+   },{
+    name:"Worash Abocherugn",
+    profilePic:"/assets/images/worash.jpeg",
+    position:"Founder and CTO",
+    rule :"Practice practice practice build skills, work smart get the job done. "
+
+   },{
+    name:"Worash Abocherugn",
+    profilePic:"/assets/images/worash.jpeg",
+    position:"Founder and CTO",
+    rule :"Practice practice practice build skills, work smart get the job done. "
+
+   }
+ ]
+ 
+ 
+ 
+ 
+ 
  let menu = document.getElementById("hamburger");
  let header =document.getElementById("main-header");
- let  mainNav =document.getElementById("main-nav")
+ let  mainNav =document.getElementById("main-nav");
+ let teamWrapper = document.getElementById("team-container");
  const sticky = header.offsetTop;
+
+
+
 
  window.onscroll = () => {
     if (window.pageYOffset >= sticky) {
@@ -17,6 +66,26 @@
      mainNav.classList.toggle("hide-menu")
  }
 
+const createTeamMembers=()=>{
+  let teams="";
+teamMembers.forEach(team=>{
+  teams += `<div class="team">
+  <img src="assets/images/chess.jpg" alt="" class="chess-img">
+  <img src="${team.profilePic}"  alt= "${team.name} profile" class="profile-img">
+  <div class="team-descption">
+      <h3 class="team-name"> ${team.name} </h3>
+      <p class="position">${team.position}</p>
+      <p class="rule"> ${team.rule}</p>
+  </div>
+  </div>`
+    
+});
+teamWrapper.innerHTML =teams;
+}
+
+
  menu.addEventListener("click",toggleMenu)
 
 
+
+ createTeamMembers();
