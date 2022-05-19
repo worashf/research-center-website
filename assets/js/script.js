b623d8cc5837dd8af6c8c500d71dfd3ba8738b60
worashf <uppert83@gmail.com>
2022-05-19 16:28:48 +0300

    add hamburger menu on scroll hide feature

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,75 +1,73 @@
- const teamMembers =[
-   {
-      name:"Worash Abocherugn",
-      profilePic:"/assets/images/worash.jpeg",
-      position:"Founder and CTO",
-      rule :"Practice practice practice build skills, work smart get the job done. "
+const teamMembers = [
+  {
+    name: 'Worash Abocherugn',
+    profilePic: '/assets/images/worash.jpeg',
+    position: 'Founder and CTO',
+    rule: 'Practice practice practice build skills, work smart get the job done. ',
 
+  }, {
+    name: 'Dagmawi zewudu',
+    profilePic: '/assets/images/dagm.jpeg',
+    position: 'Founder and CTO',
+    rule: 'Practice practice practice build skills, work smart get the job done. ',
 
-   },{
-    name:"Dagmawi zewudu",
-    profilePic:"/assets/images/dagm.jpeg",
-    position:"Founder and CTO",
-    rule :"Practice practice practice build skills, work smart get the job done. "
+  }, {
+    name: 'Addis Kebede',
+    profilePic: '/assets/images/addis.jpeg',
+    position: 'Founder and CTO',
+    rule: 'Practice practice practice build skills, work smart get the job done. ',
 
-   },{
-    name:"Addis Kebede",
-    profilePic:"/assets/images/addis.jpeg",
-    position:"Founder and CTO",
-    rule :"Practice practice practice build skills, work smart get the job done. "
+  }, {
+    name: 'Sntayhu Boyamo',
+    profilePic: '/assets/images/snte.jpeg',
+    position: 'Founder and CTO',
+    rule: 'Practice practice practice build skills, work smart get the job done. ',
 
-   },{
-    name:"Sntayhu Boyamo",
-    profilePic:"/assets/images/snte.jpeg",
-    position:"Founder and CTO",
-    rule :"Practice practice practice build skills, work smart get the job done. "
+  }, {
+    name: 'Worash Abocherugn',
+    profilePic: '/assets/images/worash.jpeg',
+    position: 'Founder and CTO',
+    rule: 'Practice practice practice build skills, work smart get the job done. ',
 
-   },{
-    name:"Worash Abocherugn",
-    profilePic:"/assets/images/worash.jpeg",
-    position:"Founder and CTO",
-    rule :"Practice practice practice build skills, work smart get the job done. "
+  }, {
+    name: 'Worash Abocherugn',
+    profilePic: '/assets/images/worash.jpeg',
+    position: 'Founder and CTO',
+    rule: 'Practice practice practice build skills, work smart get the job done. ',
 
-   },{
-    name:"Worash Abocherugn",
-    profilePic:"/assets/images/worash.jpeg",
-    position:"Founder and CTO",
-    rule :"Practice practice practice build skills, work smart get the job done. "
+  },
+];
 
-   }
- ]
- 
- 
- 
- 
- 
- let menu = document.getElementById("hamburger");
- let header =document.getElementById("main-header");
- let  mainNav =document.getElementById("main-nav");
- let teamWrapper = document.getElementById("team-container");
- const sticky = header.offsetTop;
+const menu = document.getElementById('hamburger');
+const header = document.getElementById('main-header');
+const mainNav = document.getElementById('main-nav');
+const teamWrapper = document.getElementById('team-container');
+const sticky = header.offsetTop;
 
+let prevScrollpos = window.pageYOffset;
 
+const currentScrollPos = window.pageYOffset;
+window.onscroll = () => {
+  if (window.pageYOffset >= sticky || prevScrollpos > currentScrollPos) {
+    menu.style.top = '30px';
+    header.style.top = 0;
+  } else {
+    menu.style.top = '-50px';
+    header.style.top = '5%';
+  }
+  prevScrollpos = currentScrollPos;
+};
 
+function toggleMenu() {
+  header.classList.toggle('menu-overlay');
+  menu.classList.toggle('open');
+  mainNav.classList.toggle('hide-menu');
+}
 
- window.onscroll = () => {
-    if (window.pageYOffset >= sticky) {
-      header.style.top = 0;
-    } else {
-      header.style.top = '5%';
-    }
-  };
-
- function toggleMenu(){
-     header.classList.toggle("menu-overlay")
-     menu.classList.toggle("open")
-     mainNav.classList.toggle("hide-menu")
- }
-
-const createTeamMembers=()=>{
-  let teams="";
-teamMembers.forEach(team=>{
-  teams += `<div class="team">
+const createTeamMembers = () => {
+  let teams = '';
+  teamMembers.forEach((team) => {
+    teams += `<div class="team">
   <img src="assets/images/chess.jpg" alt="" class="chess-img">
   <img src="${team.profilePic}"  alt= "${team.name} profile" class="profile-img">
   <div class="team-descption">
@@ -77,15 +75,11 @@ teamMembers.forEach(team=>{
       <p class="position">${team.position}</p>
       <p class="rule"> ${team.rule}</p>
   </div>
-  </div>`
-    
-});
-teamWrapper.innerHTML =teams;
-}
+  </div>`;
+  });
+  teamWrapper.innerHTML = teams;
+};
 
+menu.addEventListener('click', toggleMenu);
 
- menu.addEventListener("click",toggleMenu)
-
-
-
- createTeamMembers();
+createTeamMembers();
